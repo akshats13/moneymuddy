2b228667794107d1925a71d58cc974955ea25e89
akshats13 <optimisticazure@gmail.com>
2023-04-13 15:50:45 +0530

    changes

---
+++ server/models/OverallStat.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const OverallStatSchema = new mongoose.Schema(
+  {
+    totalCustomers: Number,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+      {
+        month: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: [
+      {
+        date: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    salesByCategory: {
+      type: Map,  //object creation using Map
+      of: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const OverallStat = mongoose.model("OverallStat", OverallStatSchema);
+export default OverallStat;
